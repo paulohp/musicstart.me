25d672cf5a3dff9d8fa2c65758c390eb117fb672
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-02-25 19:36:16 -0300

    addind the edit mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,28 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @projects = @user.projects.order("updated_at DESC")
+    @users = @user.users.order("updated_at DESC")
     respond_to do |format|
       format.html
       format.js
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'Task was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 end

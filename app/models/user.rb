4c21a0efab736fb0b8fa519cf441d3e61dd13baa
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-04-04 17:34:40 -0300

    added nickname to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me
+  attr_accessible :name, :nickname, :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  validates_uniqueness_of :email, :nickname, case_sensitive: false
+  validates :nickname, format: { with: /\A[\w-]+\Z/i, message: "must contain only numbers, letters and - , _ characters" }, length: { in: 2..20 }, presence: true, uniqueness: true
 end

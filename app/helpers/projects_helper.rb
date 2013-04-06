411ab40b910a53a8f62796ea4f60902ee13ea212
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-04-06 12:37:40 -0300

    adicionado imagem na exibição do projeto

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -2,4 +2,17 @@ module ProjectsHelper
   def submit_form text, finder
     link_to_function text, "$('finder').submit()"
   end
+
+  def picture_url
+    @project.image_url_url
+  end
+
+  def thumb_url
+    @project.project.thumb.url
+  end
+
+  def has_picture?
+    @project.image_url?
+  end
+
 end

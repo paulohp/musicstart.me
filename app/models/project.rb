55095dd7d34c3cf3b3bb5b0a757e012187b6cab5
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-04-06 12:23:20 -0300

    added the mount uploader in model class and the field in form

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :users
-  attr_accessible :category, :decription, :headline, :name, :price, :soundcloud_url, :user_id, :approved
+  attr_accessible :category, :decription, :headline, :name, :price, :soundcloud_url, :user_id, :approved, :image_url
+
+  mount_uploader :image_url, ProjectPictureUploader
 end

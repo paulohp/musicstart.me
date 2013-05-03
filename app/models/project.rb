8e86a7ad2cc54624e9fba9fc49c0582c59b494d6
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-05-02 23:19:35 -0300

    archives and changes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   belongs_to :users
-  attr_accessible :category, :decription, :headline, :name, :price, :soundcloud_url, :user_id, :approved, :image_url
+  attr_accessible :category, :decription, :headline, :name, :price, :soundcloud_url, :user_id, :approved, :image_url, :online_day
 
   mount_uploader :image_url, ProjectPictureUploader
 

fa15ab37b97d9354d8ba255b3a564cf9bf5d3c44
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-05-15 20:19:50 -0300

    mudanças nas partials de projetos

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,11 +1,19 @@
 class Project < ActiveRecord::Base
   belongs_to :users
+
   attr_accessible :category, :decription, :headline, :name, :price, :soundcloud_url, :user_id, :approved, :image_url, :online_day
+
+  validates_presence_of :name, :price
 
   mount_uploader :image_url, ProjectPictureUploader
 
   def owner
-    User.find(self.user_id).name
+    user = User.find(self.user_id)
+    if user.name != ""
+      return user.email
+    else
+      return user.name
+    end
   end
 
   def status_progress
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,49 +1,7 @@
 class User < ActiveRecord::Base
-has_many :points  
-has_many :badges , :through => :levels 
-has_many :levels  
-
-def change_points(options)
-  if Gioco::Core::TYPES
-    type_id = options[:type]
-    points  = options[:points]
-  else
-    points  = options
-  end
-  type      = (type_id) ? Type.find(type_id) : false
-
-  if Gioco::Core::TYPES
-    raise "Missing Type Identifier argument" if !type_id
-    old_pontuation  = self.points.where(:type_id => type_id).sum(:value)
-  else
-    old_pontuation  = self.points.to_i
-  end
-  new_pontuation    = old_pontuation + points
-  Gioco::Core.sync_resource_by_points(self, new_pontuation, type)
-end
-
-def next_badge?(type_id = false)
-  type              = (type_id) ? Type.find(type_id) : false
-  if Gioco::Core::TYPES
-    raise "Missing Type Identifier argument" if !type_id
-    old_pontuation  = self.points.where(:type_id => type_id).sum(:value)
-  else
-    old_pontuation  = self.points.to_i
-  end
-  next_badge       = Badge.where("points > #{old_pontuation}").order("points ASC").first
-  last_badge_point = self.badges.last.try('points')
-  last_badge_point ||= 0
-
-  if next_badge
-    percentage      = (old_pontuation - last_badge_point)*100/(next_badge.points - last_badge_point)
-    points          = next_badge.points - old_pontuation
-    next_badge_info = { 
-                        :badge      => next_badge,
-                        :points     => points,
-                        :percentage => percentage
-                      }
-  end
-end
+  has_many :points
+  has_many :badges , :through => :levels
+  has_many :levels
   has_many :projects, :dependent => :destroy
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -56,4 +14,48 @@ end
   # attr_accessible :title, :body
   validates_uniqueness_of :email, :nickname, case_sensitive: false
   validates :nickname, format: { with: /\A[\w-]+\Z/i, message: "must contain only numbers, letters and - , _ characters" }, length: { in: 2..20 }, presence: true, uniqueness: true
+
+
+  def change_points(options)
+    if Gioco::Core::TYPES
+      type_id = options[:type]
+      points  = options[:points]
+    else
+      points  = options
+    end
+    type      = (type_id) ? Type.find(type_id) : false
+
+    if Gioco::Core::TYPES
+      raise "Missing Type Identifier argument" if !type_id
+      old_pontuation  = self.points.where(:type_id => type_id).sum(:value)
+    else
+      old_pontuation  = self.points.to_i
+    end
+    new_pontuation    = old_pontuation + points
+    Gioco::Core.sync_resource_by_points(self, new_pontuation, type)
+  end
+
+  def next_badge?(type_id = false)
+    type              = (type_id) ? Type.find(type_id) : false
+    if Gioco::Core::TYPES
+      raise "Missing Type Identifier argument" if !type_id
+      old_pontuation  = self.points.where(:type_id => type_id).sum(:value)
+    else
+      old_pontuation  = self.points.to_i
+    end
+    next_badge       = Badge.where("points > #{old_pontuation}").order("points ASC").first
+    last_badge_point = self.badges.last.try('points')
+    last_badge_point ||= 0
+
+    if next_badge
+      percentage      = (old_pontuation - last_badge_point)*100/(next_badge.points - last_badge_point)
+      points          = next_badge.points - old_pontuation
+      next_badge_info = {
+                          :badge      => next_badge,
+                          :points     => points,
+                          :percentage => percentage
+                        }
+    end
+  end
+
 end

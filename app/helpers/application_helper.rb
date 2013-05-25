f492988e224d365bb5af1a7b3cfca368de9384b6
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-05-24 21:42:41 -0300

    refactor the helper and add one method for cancel action

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png"
   end
+
+  def submit_form text, finder
+    link_to_function text, "$('#{finder}').submit()"
+  end
+
+  def cancel_form text, local
+    link_to_function text, "window.redirect = #{local}"
+  end
 end
--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,3 @@
 module ProjectsHelper
-  def submit_form text, finder
-    link_to_function text, "$('finder').submit()"
-  end
+
 end

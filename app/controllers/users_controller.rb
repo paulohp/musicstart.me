cd4a066e439fdba696fafc8ff12596411d60471e
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-03 20:02:12 -0300

    after fds

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @projects = @user.projects.order("updated_at DESC")
+
     respond_to do |format|
       format.html
       format.js

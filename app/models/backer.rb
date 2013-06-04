6757d3c1f3c6a90426d7e57d85433257dcef7e80
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-03 21:37:43 -0300

    added rewards to backers page

--- app/models/backer.rb
+++ app/models/backer.rb
@@ -1,5 +1,6 @@
 class Backer < ActiveRecord::Base
   belongs_to :projects
   belongs_to :users
+  belongs_to :rewards
   attr_accessible :confirmed, :project_id, :reward_id, :user_id, :value, :reward_value
 end
--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,4 +2,5 @@ class Reward < ActiveRecord::Base
   attr_accessible :description, :maximum_backers, :minimum_value, :project_id
 
   belongs_to :projects
+  has_many :backers
 end

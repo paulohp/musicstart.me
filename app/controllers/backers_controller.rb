9bef056aa1a3d243314d4177a583713396f4f8f5
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-06-03 22:36:57 -0300

    arumando contador de pontos

--- app/controllers/backers_controller.rb
+++ app/controllers/backers_controller.rb
@@ -1,6 +1,12 @@
 class BackersController < InheritedResources::Base
   before_filter :authenticate_user!
 
+  def index
+    respond_to do |format|
+      format.html { redirect_to root_path }
+    end
+  end
+
   def new
     @backer = current_user.backers.build
     @project = Project.find(params[:project_id])
@@ -11,9 +17,10 @@ class BackersController < InheritedResources::Base
   # POST /tasks.json
   def create
     @backer = current_user.backers.build(params[:backer])
-
+    @user = current_user
     respond_to do |format|
       if @backer.save
+        @user.change_points({ points: 70, type: 1 })
         format.html { redirect_to root_path, notice: 'Backer was successfully created.' }
         format.json { render json: @backer, status: :created, location: @backer }
       else

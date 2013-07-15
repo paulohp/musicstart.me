e8d66a08d0a4c8777952dd81000d5de706e5bc7c
Paulo Henrique <taralloqueiroiz@gmail.com>
2013-07-15 18:45:49 -0300

    added the backers controller

--- app/controllers/backers_controller.rb
+++ app/controllers/backers_controller.rb
@@ -19,6 +19,34 @@ class BackersController < InheritedResources::Base
   def create
     @backer = current_user.backers.build(params[:backer])
     @user = current_user
+
+    payment = PagSeguro::PaymentRequest.new
+    payment.reference = order.id
+    payment.notification_url = notifications_url
+    payment.redirect_url = processing_url
+
+    order.products.each do |product|
+      payment.items << {
+        id: product.id,
+        description: product.title,
+        amount: product.price,
+        weight: product.weight
+      }
+    end
+
+    response = payment.register
+
+    # Caso o processo de checkout tenha dado errado, lança uma exceção.
+    # Assim, um serviço de rastreamento de exceções ou até mesmo a gem
+    # exception_notification poderá notificar sobre o ocorrido.
+    #
+    # Se estiver tudo certo, redireciona o comprador para o PagSeguro.
+    if response.errors.any?
+      raise response.errors.join("\n")
+    else
+      redirect_to response.url
+    end
+    
     respond_to do |format|
       if @backer.save
         @user.change_points({ points: 70, type: 1 })
---
+++ config/initializers/pagseguro.rb
@@ -0,0 +1,4 @@
+PagSeguro.configure do |config|
+  config.token = "F4F0F572297840A6BE0C9312F16FD0F4"
+  config.email = "tarallo.php@gmail.com"
+end
